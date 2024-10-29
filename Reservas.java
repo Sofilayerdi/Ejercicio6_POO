0addbc8cbdd56fae92c97172b86f2c9be9c64b06
KristCg <kriscastillo515@gmail.com>
2024-10-29 17:13:15 -0600

    Correcion de errores en la clase Resevas

--- Reservas.java
+++ Reservas.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +13,9 @@ public class Reservas{
     private int noches;
     private int personas;
     private int costoTotal;
-    private Date fecha;
+    private LocalDate fecha;
 
-    public Reservas(String nombre, String habitacion, int noches, int personas, Date fecha, int costoTotal){
+    public Reservas(String nombre, String habitacion, int noches, int personas, LocalDate fecha, int costoTotal){
         this.nombre = nombre;
         this.habitacion = habitacion;
         this.noches = noches;
@@ -21,8 +23,6 @@ public class Reservas{
         this.fecha = fecha;
         this.costoTotal = costoTotal;
     }
-
-
 
     public String getNombre() {
         return nombre;
@@ -39,7 +39,7 @@ public class Reservas{
     public int getCostoTotal() {
         return costoTotal;
     }
-    public Date getFecha() {
+    public LocalDate getFecha() {
         return fecha;
     }
 
@@ -49,6 +49,7 @@ public class Reservas{
         ArrayList<Reservas> reservas = new ArrayList<>();
         String linea;
         String separador = ",";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             while ((linea = br.readLine()) != null) {
@@ -58,9 +59,9 @@ public class Reservas{
                 int noches = Integer.parseInt(datos[2]);
                 int personas = Integer.parseInt(datos[3]);
                 int costo_total = Integer.parseInt(datos[4]);
-                String fecha = datos[5];
+                LocalDate fecha = LocalDate.parse(datos[5], formatter);
 
-                Reservas reserva = new Reservas(nombre, habitacion, noches, personas, costo_total, fecha);
+                Reservas reserva = new Reservas(nombre, habitacion, noches, personas, fecha, costo_total);
                 reservas.add(reserva);
             }
         } catch (IOException e) {
@@ -70,8 +71,9 @@ public class Reservas{
         return reservas;
     }
 
-    public void GuardarReservass(List<Reservas> reserva) {
+    public void GuardarReservas(List<Reservas> reservas) {
         String archivoCSV = "Reservas.csv";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         try (FileWriter writer = new FileWriter(archivoCSV)) {
             for (Reservas reserva : reservas) {
                 writer.append(reserva.getNombre());
@@ -82,9 +84,9 @@ public class Reservas{
                 writer.append(",");
                 writer.append(String.valueOf(reserva.getPersonas()));
                 writer.append(",");
-                writer.append(String.valueOf(reserva.getCosto_total()));
+                writer.append(String.valueOf(reserva.getCostoTotal()));
                 writer.append(",");
-                writer.append(reserva.getFecha());
+                writer.append(reserva.getFecha().format(formatter));
                 writer.append("\n");
             }
         } catch (IOException e) {
