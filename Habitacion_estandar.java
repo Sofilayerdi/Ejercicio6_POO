def14b0fd782d49719659d3dad6b60144049e90a
KristCg <kriscastillo515@gmail.com>
2024-10-29 17:14:09 -0600

    Correcion de errores en la clase Habitacion estandar

--- Habitacion_estandar.java
+++ Habitacion_estandar.java
@@ -2,24 +2,19 @@ public class Habitacion_estandar implements Habitacion{
     private int tarifaBase;
 
     
-    public Habitacion_(){
-    
+    public Habitacion_estandar(){
         this.tarifaBase = 30;
     }
-
-    
     
     public int getTarifaBase() {
         return tarifaBase;
     }
 
-
     @Override
-    public int calcular_costo(int noches, int personas);{
-        int costo_base = tarifaBase*noches;
-
-        int costo_total = costo_base;
-
+    public int calcular_costo(int noches, int personas){
+        int costo_base = (tarifaBase * noches);
+        int costo_total = (costo_base);
+    
         return costo_total;
     }
 }
