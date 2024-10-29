5391e031d891dfa77a1b28d14dbd6c4dcd2d0d74
KristCg <kriscastillo515@gmail.com>
2024-10-29 17:12:15 -0600

    Correciones de errores en la clase Habitacion Ejecutiva

--- Habitacion_ejecutiva.java
+++ Habitacion_ejecutiva.java
@@ -8,35 +8,28 @@ public class Habitacion_ejecutiva implements Habitacion{
         this.serviciosDesayuno = 15;
         this.serviciosLavanderia = 10;
     }
-
-    
     
     public int getTarifaBase() {
         return tarifaBase;
     }
 
-
-
     public int getServiciosDesayuno() {
         return serviciosDesayuno;
     }
-
-
 
     public int getServiciosLavanderia() {
         return serviciosLavanderia;
     }
 
-
-
     @Override
-    public int calcular_costo(int noches, int personas);{
-        int costo_base = tarifaBase * noches;
-        int serviviosD = serviciosDesayuno * persona;
-        int serviciosL = serviciosLavanderia * noches;
+    public int calcular_costo(int noches, int personas){
+        int costo_base = (tarifaBase * noches);
+        int serviviosD = (serviciosDesayuno * personas);
+        int serviciosL = (serviciosLavanderia * noches);
 
-        int costo_total = costo_base + serviciosD + serviciosL;
-
+        int costo_total = (costo_base + serviciosDesayuno + serviciosLavanderia);
+    
         return costo_total;
+
     }
 }
